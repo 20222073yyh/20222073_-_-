7a201f003022c937708b48164d69a113a580515c
20222073yyh <yyh0377@naver.com>
2024-04-19 09:50:10 +0900

    .

---
+++ border.cpp
@@ -0,0 +1,94 @@
+#include<string>
+#include<vector>
+#include<iostream>
+
+#include "split.h"
+
+using namespace std;
+
+
+string::size_type width(const vector<string>& v) {
+	string::size_type maxlen = 0;
+
+	for (vector<string>::size_type i = 0; i != v.size(); i++) {
+		maxlen = max(maxlen, v[i].size());
+	}
+
+	return maxlen;
+}
+
+
+vector<string> frame(const vector<string>& v) {
+	vector<string> ret;
+	string::size_type maxlen = width(v);
+	string border(maxlen + 4, ' *');
+
+	//상단 테두리를 추가
+	ret.push_back(border);
+
+	//양 끝이 별표와 공백으로 둘러싸인
+	//문자열들을 각각 추가
+	for (vector<string>::size_type i = 0;
+		i != v.size(); i++) {
+		ret.push_back("*" + v[i]+
+			string(maxlen - v[i].size(), ' ') + "*");
+	}
+
+
+	ret.push_back(border);
+	return ret;
+}
+
+//수평 결합
+vector<string> vcat(const vector<string>& top,
+	const vector <string>& bottom) {
+	vector <string> ret = top;
+	//아래쪽 문자 그림을 복사
+	for (vector<string>::const_iterator it = bottom.begin();
+		it != bottom.end(); ++it) {
+		ret.push_back(*it);
+	}
+
+	return ret;
+}
+
+vector<string> hcat(const vector<string>& left,
+	const vector <string>& right) {
+	vector<string> ret;
+
+	string::size_type width1 = width(left) + 1;
+
+	vector<string>::size_type i = 0, j = 0;
+
+	while (i != left.size() || j != right.size()) {
+		string s;
+
+		if (i != left.size()) {
+			s = left[i++];
+		}
+
+		s += string(width1 - s.size(), ' ');
+
+		if (j != right.size()) {
+			s += right[j++];
+		}
+
+		ret.push_back(s);
+	}
+	return ret;
+}
+
+//과제 1.split()가져오고 사용하기
+//2. vcat()사용하기
+//3.hcat() 사용하기
+//4.out.txt 파일레서 모든 출력 저장해주세요
+//int main() {
+//	string s;
+//
+//	while (getline(cin, s)) {
+//		vector<string> v = split(s);
+//		frame(v);
+//	}
+//
+//	return 0;
+//}
